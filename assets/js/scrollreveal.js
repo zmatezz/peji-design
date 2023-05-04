056e640deb1969de3c2e18483b9f4e5abb4dde42
Matheus - Aula 1 <matheusguedescosta@gmail.com>
2023-05-03 23:13:51 -0300

    first commit

---
+++ assets/js/scrollreveal.js
@@ -0,0 +1,31 @@
+window.sr = ScrollReveal({ reset: true })
+sr.reveal(".top-left", {
+  origin: "left", distance: "100px", duration: 2000,
+  easing: 'ease-in-out',
+});
+
+sr.reveal(".top-right", {
+  origin: "top", distance: "100px", duration: 2000,
+  easing: 'ease-in-out',
+});
+
+sr.reveal(".bottom-left", {
+  origin: "bottom", distance: "100px", duration: 2000,
+  easing: 'ease-in-out',
+});
+
+sr.reveal(".top-left", {
+  origin: "left", distance: "100px", duration: 2000,
+  easing: 'ease-in-out',
+});
+
+sr.reveal(".bottom-right", {
+  origin: "right", distance: "100px", duration: 2000,
+  easing: 'ease-in-out',
+});
+
+sr.reveal(".square", {
+  origin: "top", distance: "40px", duration: 2000,
+  easing: 'ease-in-out',
+});
+
