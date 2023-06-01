54faa6b18954460399ee16d0ab448d26455ddf1c
Matheus - Aula 1 <matheusguedescosta@gmail.com>
2023-06-01 00:18:07 -0300

    scroll

--- assets/js/about-me.js
+++ assets/js/about-me.js
@@ -5,3 +5,28 @@ menu.onclick = () => {
   menu.classList.toggle('bx-x');
   navbar.classList.toggle('open');
 }
+
+$(document).ready(function() {
+  var startX = 0;
+  var scrollLeft = 0;
+  var isDragging = false;
+
+  $(".scroll-container").on("mousedown", function(e) {
+    e.preventDefault();
+    isDragging = true;
+    startX = e.pageX - $(this).offset().left;
+    scrollLeft = $(this).scrollLeft();
+  }).on("mousemove", function(e) {
+    if (isDragging) {
+      var moveX = e.pageX - $(this).offset().left - startX;
+      $(this).scrollLeft(scrollLeft - moveX);
+    }
+  }).on("mouseup mouseleave", function() {
+    isDragging = false;
+  }).on("wheel", function(e) {
+    e.preventDefault();
+    var delta = e.originalEvent.deltaY || e.originalEvent.wheelDelta;
+    $(this).scrollLeft($(this).scrollLeft() + delta);
+  });
+});
+
