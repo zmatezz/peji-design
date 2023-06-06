1e6c3f3f1cf2445d0130795f14284fc7ce03a3ee
Matheus - Aula 1 <matheusguedescosta@gmail.com>
2023-06-05 23:48:47 -0300

    develop services

--- assets/js/about-me.js
+++ assets/js/about-me.js
@@ -30,3 +30,9 @@ $(document).ready(function() {
   });
 });
 
+// header.scroll
+const header = document.querySelector("header");
+window.addEventListener ("scroll", function(){
+  header.classList.toggle ("sticky", window.scrollY > 100)
+}
+)
